7a8244bd051b167dfb7c3b18ab01b1adc8c4d7fb
Arran France <arranf@gmail.com>
2015-12-16 03:07:23 +0000

    Add new column, fix a missing ", and move working ghost js into external file

--- js/ghost.js
+++ js/ghost.js
@@ -1,14 +1,5 @@
-(function() {
-  //var clientSecret = "9708c4b45e81"; //real
-//var clientSecret = "76d12d0ddfaf";
-  //var clientId = "ghost-frontend";
-  $.get('http://blog.arranfrance.com/ghost/api/v0.1/posts?client_id=my-app&client_secret=XWfmJKYYh9', function(json) {
-        alert("data.posts[0].title");
-  }, "json");
-
-  /*$.getJSON('http://blog.arranfrance.com/ghost/api/v0.1/posts?client_id=my-app&client_secret=XWfmJKYYh9', function(data){
-      alert("data.posts[0].title")
-    //document.getElementById("test").innerHTML=data.posts[0].title;
-  });*/
-
-})();
+$(document).on('ready', function(){
+  $.get('http://blog.arranfrance.com/ghost/api/v0.1/posts/?client_id=my-app&client_secret=XWfmJKYYh9', function (json) {
+    console.log('test', json);
+  });
+});
