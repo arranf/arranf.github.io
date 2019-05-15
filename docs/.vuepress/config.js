99f416b3fef30cd3ff6091e10684118cdfa81253
Arran France <arran@arranfrance.com>
2019-05-15 08:30:35 +0100

    Add film list

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,22 +1,22 @@
 module.exports = {
-    title: 'Arran France',
-    description: 'Arran France\'s Personal Site',
-    themeConfig: {
-      search: false,
-      lastUpdated: 'Last Updated',
-      serviceWorker: {
-        updatePopup: { 
-            message: "Changes have been made.", 
-            buttonText: "Refresh" 
-        }
-      },
-      nav: [
-        { text: 'Home', link: '/' },
-        { text: 'CV', link: '/cv/' },
-        { text: 'Github', link: 'https://github.com/arranf' },
-        { text: 'Twitter', link: 'https://twitter.com/arranf' },
-        { text: 'Blog', link: 'https://blog.arranfrance.com' },
-      ]
-    }
+  title: 'Arran France',
+  description: 'Arran France\'s Personal Site',
+  themeConfig: {
+    search: false,
+    lastUpdated: 'Last Updated',
+    serviceWorker: {
+      updatePopup: {
+        message: "Changes have been made.",
+        buttonText: "Refresh"
+      }
+    },
+    nav: [
+      { text: 'Home', link: '/' },
+      { text: 'CV', link: '/cv/' },
+      { text: 'Films', link: '/films/' },
+      { text: 'Github', link: 'https://github.com/arranf' },
+      { text: 'Twitter', link: 'https://twitter.com/arranf' },
+      { text: 'Blog', link: 'https://blog.arranfrance.com' },
+    ]
   }
-  
+}
