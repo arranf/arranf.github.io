fe02974a974d1e8ede02daff0afa4d320aa06f5a
Arran <arran@arranfrance.com>
2020-05-31 22:13:07 +0100

    Remove: PopUp fade

--- docs/.vuepress/components/PopUp.vue
+++ docs/.vuepress/components/PopUp.vue
@@ -1,12 +1,8 @@
 <template>
   <transition name="sw-update-popup">
-    <div
-      class="sw-update-popup" :class="classes"
-    >
+    <div class="sw-update-popup" :class="classes">
       <p>{{message}}</p>
-      <a class="download-link" :href="url">
-        {{buttonText}}
-      </a>
+      <a class="download-link" :href="url">{{buttonText}}</a>
     </div>
   </transition>
 </template>
@@ -19,93 +15,100 @@ export default {
       type: String
     }
   },
-  data() {
-    return {
-      isFaded: false
-    }
-  },
+  // data() {
+  //   return {
+  //     isFaded: false
+  //   };
+  // },
   computed: {
-    message () {
-      return  'Also available as a PDF'
+    message() {
+      return "Also available as a PDF";
     },
-    buttonText () {
-      return 'Download'
+    buttonText() {
+      return "Download";
     },
     classes() {
       return {
-        'faded': this.isFaded
-      }
+        // faded: this.isFaded
+      };
     }
   },
   methods: {
-    setFaded() {
-      this.isFaded = true
-    }
+    // setFaded() {
+    // // If you want to reenable the fading of the popup uncomment this line
+    // this.isFaded = true
+    // }
   },
   created() {
-    setTimeout(this.setFaded, 3000)
+    // setTimeout(this.setFaded, 3000);
   }
-}
+};
 </script>
 
 <style lang="stylus" scoped>
-$accentColor = #3eaf7c
-$codeBgColor = #282c34
+$accentColor = #3eaf7c;
+$codeBgColor = #282c34;
 
-.sw-update-popup
-    display flex
-    position fixed
-    padding 0.65em
-    border none
-    border-radius 3px
-    background $codeBgColor
-    box-shadow 0 4px 16px rgba(0, 0, 0, 0.5)
+.sw-update-popup {
+  display: flex;
+  position: fixed;
+  padding: 0.65em;
+  border: none;
+  border-radius: 3px;
+  background: $codeBgColor;
+  box-shadow: 0 4px 16px rgba(0, 0, 0, 0.5);
+  // Mobile specific here
+  min-width: 100%;
+  left: 0;
+  bottom: 0;
+}
 
+p, a {
+  flex: 1;
+  font-size: 90%;
+  align-self: center;
+  text-align: left;
+}
+
+.download-link {
+  text-align: right;
+  padding-right: 1.5em;
+}
+
+p {
+  color: #ffffff;
+  margin: 0 0;
+  padding-left: 0.2em;
+  white-space: nowrap;
+}
+
+@media (min-width: 768px) {
+  .sw-update-popup {
+    text-align: left;
     // Mobile specific here
-    min-width 100%
-    left 0
-    bottom 0
+    left: initial;
+    min-width: initial;
+    right: 0.2em;
+    bottom: 0.2em;
+  }
 
-  p, a
-    flex 1
-    font-size 90%
-    align-self center
-    text-align left
-    
-  
-  .download-link
-    text-align right
-    padding-right 1.5em
+  // .faded {
+  // transition: opacity 1s ease;
+  // opacity: 0.3;
 
-  p
-    color: #ffffff
-    margin: 0 0
-    padding-left: 0.2em
-    white-space nowrap
-  
+  // &:hover {
+  // transition: none;
+  // opacity: 1;
+  // }
+  // }
+  p, a {
+    margin-left: 1em;
+  }
+}
 
-@media (min-width: 768px)
-  .sw-update-popup
-    text-align left
-
-    // Mobile specific here
-    left initial
-    min-width initial
-    right 0.2em
-    bottom 0.2em
-
-
-  .faded
-    transition: opacity 1s ease;
-    opacity 0.3
-    &:hover
-      transition none
-      opacity 1
-
-  p,a
-    margin-left 1em
-
-@media print 
-  .sw-update-popup
-    display none
+@media print {
+  .sw-update-popup {
+    display: none;
+  }
+}
 </style>
